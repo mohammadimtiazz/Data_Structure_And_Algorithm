f631faa09b86c7f3cde05e5ddad9141a6792ce87
Mohammad Imtiaz <mohammad.imtiazz@gmail.com>
2019-09-16 10:58:57 -0500

    Added Brute force method of sub string search

--- SubStringSearch/SubStringSearch/main.cpp
+++ SubStringSearch/SubStringSearch/main.cpp
@@ -73,12 +73,59 @@ int RobinKarpSubStringSearch(string text, string pattern, long key) {
 
 
 
+int BruteSearch(string pattern, string text) {
+	int count = 0;
+
+	// first for loop for pointing from start to end length
+	for (int i = 0; i <= text.length() - pattern.length(); i++) {
+
+		// Next for loop used for slicing word from the text in same length as pattern size
+		for (int j = i; j < pattern.length()+i; j++) {
+			
+			count = 0;
+
+			//Matching the pattern with the sliced word from text
+			while (count < pattern.length()) {
+				if (pattern[count] != text[j + count]) {
+					break;
+				}
+				count++;
+			}
+
+			
+			if (count == pattern.length()) {	//Match condition
+				cout << "found a match at index " << j << endl;
+				break;
+			}
+			else {	//No match condition
+				break;		
+			}
+						
+		}
+
+	}
+
+	return 1;
+}
+
+
 int main() {
 	string pattern = "26535";
-	//string text = "3141592653589793";
 	string text = "265353141592653589793";
-	long key = 997;		//this key have to be a large random prime number
 
+	//string pattern = "abc";
+	//string text = "babcdeabc";
+
+	cout << "Given pattern is ::: " << pattern << endl;
+	cout << "Given text is :::: " << text << endl;
+	
+	cout << "Executing pattern search in text using Brute Force method" << endl;
+	BruteSearch(pattern, text);
+
+
+
+	cout << "Executing pattern search in text using Rabin-karp method" << endl;
+	long key = 997;		//this key have to be a large random prime number
 	RobinKarpSubStringSearch(text,pattern,key);
 
 	return 1;
